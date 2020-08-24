8f01710b7a7c8e105df210dd4f1cb481492d180f
SGCMarkus <markusornik@gmail.com>
2020-08-24 17:11:05 +0200

    eqs: implement lineage touch hal

--- device.mk
+++ device.mk
@@ -98,6 +98,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/android.hardware.sensors@2.1-service-multihal.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.sensors@2.1-service-multihal.rc \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
+# Touch
+# PowerShare
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.eqs
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
