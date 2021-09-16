4548ac73de7992a5e54809563013adab789c2f37
LuK1337 <priv.luk@gmail.com>
2021-09-16 13:55:07 +0200

    eqs: Implement vendor.lineage.powershare@1.0 HAL
    Change-Id: I8022ac47411369f3ede969417fd46e08fb984e24

--- device.mk
+++ device.mk
@@ -86,6 +86,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml
 
+# PowerShare
+PRODUCT_PACKAGES += \
+    vendor.lineage.powershare@1.0-service.eqs
+
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.eqs
