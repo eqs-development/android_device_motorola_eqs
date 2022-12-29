7fd5429a5e90744ccff4adf2e28c4ef1fae63451
SGCMarkus <markusornik@gmail.com>
2022-12-29 18:18:38 +0100

    eqs: update fringerprint/sec patch, and pull from S3SQ32.16-72-14-2

--- lineage_eqs.mk
+++ lineage_eqs.mk
@@ -34,6 +34,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=eqs_ge \
-    PRIVATE_BUILD_DESC="eqs_ge-user 12 S3SQ32.16-72-14 896aec-56d40 release-keys"
+    PRIVATE_BUILD_DESC="eqs_ge-user 12 S3SQ32.16-72-14-2 7bb28-87384 release-keys"
 
-BUILD_FINGERPRINT := motorola/eqs_ge/msi:12/S3SQ32M.16-72-14/896aec-56d40:user/release-keys
+BUILD_FINGERPRINT := motorola/eqs_ge/msi:12/S3SQ32M.16-72-14-2/7bb28-87384:user/release-keys
