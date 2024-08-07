6883e9b8e83f3784f2a8ad70b4fe5b275cda75cf
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-08 00:14:59 +0300

    eqs: remove device-specific touch
    Change-Id: I209faac2d80cd5ae38e514bcb2e1a04be731cd60

--- device.mk
+++ device.mk
@@ -94,10 +94,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.eqs
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
