f7b14bb0689a8e568ec36ba841104bc0e5fd96be
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-20 11:37:28 +0300

    eqs: set BOOT_SECURITY_PATCH
    Change-Id: I8ae16175a4f471098326e920bfdf42a7fa316bff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,7 +51,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 90
 
 # Security
-VENDOR_SECURITY_PATCH := 2024-06-01
+BOOT_SECURITY_PATCH := 2024-06-01
+VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # inherit from the proprietary version
 include vendor/motorola/eqs/BoardConfigVendor.mk
